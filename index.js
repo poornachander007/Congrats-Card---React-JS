a6cf655b8132618fae66f4dbf8d5f16c07ac8211
poornachander007 <poornachander0333@gmail.com>
2023-03-29 13:54:09 +0000

    solution

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div>
+    <h1 className="heading">Congratulations</h1>
+
+    <div className="card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+
+      <h2>Poorna V</h2>
+
+      <p>Poorna institute of computer education and Technology, Hyderabad.</p>
+
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
